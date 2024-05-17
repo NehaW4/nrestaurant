5f9e919b91c5a116e6cd1da9c257ab2b7c4f5013
NehaW4 <nehuwakhare04@gmail.com>
2024-05-18 03:01:26 +0530

    add file

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import React from 'react'
+import Restuarant from './componets/Basics/Restuarant';
+
+const App = () => {
+  return (
+     <Restuarant/>
+    // <UseState/>
+    // <UseEffect/>
+    // <UseReducer/>
+  )
+}
+
+export default App;
+
